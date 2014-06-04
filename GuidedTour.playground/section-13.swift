b394fd5d9542f5fa73b4254fdac4bcdd82b387b6
Petr Korolev <sky4winder@gmail.com>
2014-06-04 18:19:07 +0300

    test

--- GuidedTour.playground/section-13.swift
+++ GuidedTour.playground/section-13.swift
@@ -1,8 +1,12 @@
 var shoppingList = ["catfish", "water", "tulips", "blue paint"]
 shoppingList[1] = "bottle of water"
 
+shoppingList
+
 var occupations = [
     "Malcolm": "Captain",
     "Kaylee": "Mechanic",
  ]
 occupations["Jayne"] = "Public Relations"
+
+occupations
