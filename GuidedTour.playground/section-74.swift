66202758386deeed3beffa7f4e41d9943603bd91
Petr Korolev <sky4winder@gmail.com>
2014-10-02 14:06:36 +0300

    changes

--- GuidedTour.playground/section-74.swift
+++ GuidedTour.playground/section-74.swift
@@ -1,14 +1,20 @@
 enum ServerResponse {
     case Result(String, String)
     case Error(String)
+    case Ubknown(Int)
 }
  
 let success = ServerResponse.Result("6:00 am", "8:09 pm")
 let failure = ServerResponse.Error("Out of cheese.")
+let undef = ServerResponse.Ubknown(42)
  
-switch success {
+switch undef {
 case let .Result(sunrise, sunset):
     let serverResponse = "Sunrise is at \(sunrise) and sunset is at \(sunset)."
 case let .Error(error):
     let serverResponse = "Failure...  \(error)"
+case let .Ubknown(int):
+    let str = String(int)
+default:
+    let serverResponde = "def"
 }
